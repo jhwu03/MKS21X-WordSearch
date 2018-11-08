d3069302ba166b5fc1aed70d9e5fe025cff51b07
jwu03 <jwu03@stuy.edu>
2018-11-07 23:07:09 -0500

    finished

--- WordSearch.java
+++ WordSearch.java
@@ -28,35 +28,39 @@ public class WordSearch{
     }
 
     public boolean addWordHorizontal(String word,int row, int col){
-      if(word.length() + col >= data[row].length){
+      if(word.length() > data[row].length - col){
         return false;
       }
-      int n = 0;
-      for(int i = col; i < col + word.length();i++){
-        if(data[row][i] != '_' || data[row][i] != word.charAt(i)){
+      int n = col;
+      for(int i = 0; i < word.length();i++){
+        if(data[row][n] != '_' && data[row][n] != word.charAt(i)){
           return false;
-      }else{
-        data[row][i] = word.charAt(n);
+        }
         n = n + 1;
       }
-    }
-    return true;
-  }
-  public boolean addWordVertical(String word,int row, int col){
-    if(word.length() + row > data.length){
-      return false;
-    }else{
-      int n = 0;
-      for(int i = row; i < row + word.length();i++){
-        if(data[i][col] != '_' || data[i][col] != word.charAt(i)){
-          return false;
-        }else{
-          data[i][col] = word.charAt(n);
-          n = n + 1;
-        }
+      for(int i = 0; i < word.length(); i++){
+        data[row][col] = word.charAt(i);
+        col = col + 1;
       }
       return true;
     }
+
+  public boolean addWordVertical(String word,int row, int col){
+    if(word.length() > data.length - row ){
+      return false;
+    }
+    int n = row;
+    for(int i = 0; i < word.length();i++){
+      if(data[n][col] != '_' && data[n][col] != word.charAt(i)){
+        return false;
+      }
+      n = n + 1;
+    }
+    for(int i = 0; i < word.length(); i++){
+      data[row][col] = word.charAt(i);
+      row = row + 1;
+    }
+    return true;
   }
 
 }
