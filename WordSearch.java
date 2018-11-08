6cf6a198b904258f19030400bbab6fa642771ea0
jwu03 <jwu03@stuy.edu>
2018-11-07 21:21:44 -0500

    first method

---
+++ WordSearch.java
@@ -0,0 +1,13 @@
+public class WordSearch{
+  private char[][]data;
+
+  public WordSearch(int rows,int cols){
+    data = new char[row][cols]
+    for(int i = 0; i < data.length;i++){
+      for(int n = 0; n < data[i].length;n = n + 1){
+        data[i][n] = "-";
+      }
+    }
+  }
+  
+}
