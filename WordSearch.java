fdddc6849ee14e3d849b50e6d3deb1528b116dae
jwu03 <jwu03@stuy.edu>
2018-11-19 00:46:18 -0500

    found the big error that broke the code

--- WordSearch.java
+++ WordSearch.java
@@ -9,16 +9,7 @@ public class WordSearch{
   private Random randgen;
   private ArrayList<String> wordsToAdd;
   private ArrayList<String> wordsAdded;
-
-  public WordSearch(int rows,int cols){
-    data = new char[rows][cols];
-    for(int i = 0; i < data.length;i++){
-      for(int n = 0; n < data[i].length;n = n + 1){
-        data[i][n] = '_';
-      }
-    }
-  }
-
+  
   public WordSearch( int rows, int cols, String fileName, int rseed){
     data = new char[rows][cols];
     clear();
@@ -38,7 +29,7 @@ public class WordSearch{
     }
     randgen = new Random(rseed);
     seed = rseed;
-    //addAllWords();
+    addAllWords();
   }
 
   public WordSearch( int rows, int cols, String fileName){
@@ -57,14 +48,14 @@ public class WordSearch{
       System.out.println("file" + fileName + "not found");
       System.exit(1);
     }
-    seed = (int)(Math.random()*100000);
+    seed = (int)(Math.random()*10000);
     randgen = new Random(seed);
-    //addAllWords();
+    addAllWords();
   }
 
   private void clear(){
       for (int i = 0; i < data.length; i++){
-        for (int n = 0; n < data[i].length; n++){
+        for (int n = 0; n < data[0].length; n++){
           data[i][n] = '_';
         }
       }
@@ -84,66 +75,68 @@ public class WordSearch{
           ans =  ans + wordsAdded.get(i) + ", " ;
         }
         ans = ans + wordsAdded.get(wordsAdded.size() - 1);
+
       return ans + "\n" + "seed: " + seed + " ";
     }
-
-    public boolean addWordHorizontal(String word,int row, int col){
-      if(word.length() > data[row].length - col){
-        return false;
-      }
-      int n = col;
-      for(int i = 0; i < word.length();i++){
-        if(data[row][n] != '_' && data[row][n] != word.charAt(i)){
-          return false;
-        }
-        n = n + 1;
-      }
-      for(int i = 0; i < word.length(); i++){
-        data[row][col] = word.charAt(i);
-        col = col + 1;
-      }
-      return true;
-    }
-
-  public boolean addWordVertical(String word,int row, int col){
-    if(word.length() > data.length - row ){
-      return false;
-    }
-    int n = row;
-    for(int i = 0; i < word.length();i++){
-      if(data[n][col] != '_' && data[n][col] != word.charAt(i)){
-        return false;
-      }
-      n = n + 1;
-    }
-    for(int i = 0; i < word.length(); i++){
-      data[row][col] = word.charAt(i);
-      row = row + 1;
-    }
-    return true;
-  }
-
-  public boolean addWordDiagonal(String word, int row, int col){
-    if ( word.length() > data.length - row || word.length() > data[0].length - col){
-      return false;
-    }
-    int n = col;
-    int j = row;
-    for(int i = 0; i < word.length();i++){
-      if(data[j][n] != '_' && data[j][n] != word.charAt(i)){
-        return false;
-      }
-      n = n + 1;
-      j = j + 1;
-    }
-    for(int i = 0; i < word.length(); i++){
-      data[row][col] = word.charAt(i);
-      row = row + 1;
-      col = col + 1;
-    }
-    return true;
-  }
+  //
+  //   public boolean addWordHorizontal(String word,int row, int col){
+  //     if(word.length() > data[row].length - col){
+  //       return false;
+  //     }
+  //     int n = col;
+  //     for(int i = 0; i < word.length();i++){
+  //       if(data[row][n] != '_' && data[row][n] != word.charAt(i)){
+  //         return false;
+  //       }
+  //       n = n + 1;
+  //     }
+  //     for(int i = 0; i < word.length(); i++){
+  //       data[row][col] = word.charAt(i);
+  //       col = col + 1;
+  //     }
+  //     return true;
+  //   }
+  //
+  // public boolean addWordVertical(String word,int row, int col){
+  //   if(word.length() > data.length - row ){
+  //     return false;
+  //   }
+  //   int n = row;
+  //   for(int i = 0; i < word.length();i++){
+  //     if(data[n][col] != '_' && data[n][col] != word.charAt(i)){
+  //       return false;
+  //     }
+  //     n = n + 1;
+  //   }
+  //   for(int i = 0; i < word.length(); i++){
+  //     data[row][col] = word.charAt(i);
+  //     row = row + 1;
+  //   }
+  //   return true;
+  // }
+  //
+  // public boolean addWordDiagonal(String word, int row, int col){
+  //   if ( word.length() > data.length - row || word.length() > data[0].length - col){
+  //     return false;
+  //   }
+  //   int n = col;
+  //   int j = row;
+  //   for(int i = 0; i < word.length();i++){
+  //     if(data[j][n] != '_' && data[j][n] != word.charAt(i)){
+  //       return false;
+  //     }
+  //     n = n + 1;
+  //     j = j + 1;
+  //   }
+  //   for(int i = 0; i < word.length(); i++){
+  //     data[row][col] = word.charAt(i);
+  //     row = row + 1;
+  //     col = col + 1;
+  //   }
+  //   return true;
+  // }
   public boolean addWord( String word, int r, int c, int rowIncrement, int colIncrement){
+
     word = word.toUpperCase();
     if(rowIncrement == 0 && colIncrement == 0){
       return false;
@@ -160,16 +153,23 @@ public class WordSearch{
     if (colIncrement == -1 && c < word.length() - 1 ){
          return false;
        }
-    for (int i = 0; i < word.length(); i ++){
-     if (data[i * rowIncrement][i * colIncrement] != word.charAt(i) && data[i * rowIncrement][i * colIncrement] != '_') {
-       return false;
-     }
-   }
-    for(int i = 0; i < word.length(); i ++){
-      data[i * rowIncrement][i * colIncrement] = word.charAt(i);
+  for (int i = 0; i < word.length(); i ++){
+    int row = (i * rowIncrement) + r;
+    int col = (i * colIncrement) + c;
+    if (row < 0 || col < 0){
+      return false;
     }
-    return true;
+    if (data[row][col] != word.charAt(i) && data[row][col] != '_') {
+      return false;
+    }
   }
+  for(int i = 0; i < word.length(); i ++){
+    int row = r + (i * rowIncrement);
+    int col = c + (i * colIncrement);
+        data[row][col] = word.charAt(i);
+      }
+  return true;
+}
   public void addAllWords(){
     int a = 0;
     while(a < 1000 && wordsToAdd.size() > 0){
@@ -198,11 +198,11 @@ public class WordSearch{
   }
   public void fillrand(){
     String a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int l = a.length();
     for(int i = 0 ; i < data.length ; i = i + 1){
       for(int r = 0; r < data[i].length; r = r + 1){
         if (data[i][r] == '_'){
-          data[i][r] = a.charAt(Math.abs(randgen.nextInt(l)));
+          int index = Math.abs(randgen.nextInt() % 26 );
+          data[i][r] = a.charAt(index);
         }
       }
     }
